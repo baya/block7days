35da0586ca5380ca86bf8f4da80ba179c4bda69d
baya <kayak.jiang@gmail.com>
2017-08-02 19:26:59 +0800

    update

--- protocols/beej_pack.c
+++ protocols/beej_pack.c
@@ -18,19 +18,30 @@ static unsigned char NUL_BYT_ORD = '\0';
 
 static uint64_t pack754(long double f, unsigned bits, unsigned expbits);
 static long double unpack754(uint64_t i, unsigned bits, unsigned expbits);
-void packi16(unsigned char *buf, unsigned int i, const unsigned char bytord);
+static void packi16(unsigned char *buf, unsigned int i, const unsigned char bytodr);
 static void packi16_little(unsigned char *buf, unsigned int i);
-void packi32(unsigned char *buf, unsigned long int i, const unsigned char bytord);
-void packi32_little(unsigned char *buf, unsigned long int i);
-static void packi64(unsigned char *buf, unsigned long long int i, const unsigned char bytord);
-void packi64_little(unsigned char *buf, unsigned long long int i);
-static int unpacki16(unsigned char *buf);
-static unsigned int unpacku16(unsigned char *buf);
-static long int unpacki32(unsigned char *buf);
-static unsigned long int unpacku32(unsigned char *buf);
-static long long int unpacki64(unsigned char *buf);
-static unsigned long long int unpacku64(unsigned char *buf);
-
+static void packi32(unsigned char *buf, unsigned long int i, const unsigned char bytodr);
+static void packi32_little(unsigned char *buf, unsigned long int i);
+static void packi64(unsigned char *buf, unsigned long long int i, const unsigned char bytodr);
+static void packi64_little(unsigned char *buf, unsigned long long int i);
+static int unpacki16(unsigned char *buf, const unsigned char bytodr);
+static int unpacki16_big(unsigned char *buf);
+static int unpacki16_little(unsigned char *buf);
+static unsigned int unpacku16(unsigned char *buf, const unsigned char bytodr);
+static unsigned int unpacku16_big(unsigned char *buf);
+static unsigned int unpacku16_little(unsigned char *buf);
+static long int unpacki32(unsigned char *buf, const unsigned char bytodr);
+static long int unpacki32_big(unsigned char *buf);
+static long int unpacki32_little(unsigned char *buf);
+static unsigned long int unpacku32(unsigned char *buf, const unsigned char bytodr);
+static unsigned long int unpacku32_big(unsigned char *buf);
+static unsigned long int unpacku32_little(unsigned char *buf);
+static long long int unpacki64(unsigned char *buf, const unsigned char bytodr);
+static unsigned long long int unpacku64(unsigned char *buf, const unsigned char bytodr);
+static long long int unpacki64_little(unsigned char *buf);
+static long long int unpacki64_big(unsigned char *buf);
+static long long int unpacku64_little(unsigned char *buf);
+static long long int unpacku64_big(unsigned char *buf);
 
 uint64_t pack754(long double f, unsigned bits, unsigned expbits)
 {
@@ -91,9 +102,9 @@ long double unpack754(uint64_t i, unsigned bits, unsigned expbits)
 /*
 ** packi16() -- store a 16-bit int into a char buffer (like htons())
 */ 
-void packi16(unsigned char *buf, unsigned int i, const unsigned char bytord)
+void packi16(unsigned char *buf, unsigned int i, const unsigned char bytodr)
 {
-    if(bytord == '<'){
+    if(bytodr == '<'){
 	packi16_little(buf, i);
     } else {
 	*buf++ = i>>8; *buf++ = i;
@@ -110,9 +121,9 @@ void packi16_little(unsigned char *buf, unsigned int i)
 /*
 ** packi32() -- store a 32-bit int into a char buffer (like htonl())
 */ 
-void packi32(unsigned char *buf, unsigned long int i, const unsigned char bytord)
+void packi32(unsigned char *buf, unsigned long int i, const unsigned char bytodr)
 {
-    if(bytord == '<'){
+    if(bytodr == '<'){
 	packi32_little(buf, i);
     } else {
 	*buf++ = i>>24; *buf++ = i>>16;
@@ -158,7 +169,21 @@ void packi64_little(unsigned char *buf, unsigned long long int i)
 /*
 ** unpacki16() -- unpack a 16-bit int from a char buffer (like ntohs())
 */ 
-int unpacki16(unsigned char *buf)
+int unpacki16(unsigned char *buf, const unsigned char bytodr)
+{
+    int i;
+
+    if(bytodr == '<'){
+	i = unpacki16_little(buf);
+    } else {
+	i= unpacki16_big(buf);
+    }
+
+    return i;
+}
+
+
+int unpacki16_big(unsigned char *buf)
 {
     unsigned int i2 = ((unsigned int)buf[0]<<8) | buf[1];
     int i;
@@ -170,18 +195,61 @@ int unpacki16(unsigned char *buf)
     return i;
 }
 
+int unpacki16_little(unsigned char *buf)
+{
+    unsigned int i2 = ((unsigned int)buf[1]<<8) | buf[0];
+    int i;
+
+    // change unsigned numbers to signed
+    if (i2 <= 0x7fffu) { i = i2; }
+    else { i = -1 - (unsigned int)(0xffffu - i2); }
+
+    return i;
+}
+
+
 /*
 ** unpacku16() -- unpack a 16-bit unsigned from a char buffer (like ntohs())
 */ 
-unsigned int unpacku16(unsigned char *buf)
+unsigned int unpacku16(unsigned char *buf, const unsigned char bytodr)
+{
+    unsigned int i;
+    if(bytodr == '<'){
+	i = unpacku16_little(buf);
+    } else {
+	i = unpacku16_big(buf);
+    }
+
+    return i;
+}
+
+unsigned int unpacku16_big(unsigned char *buf)
 {
     return ((unsigned int)buf[0]<<8) | buf[1];
+}
+
+unsigned int unpacku16_little(unsigned char *buf)
+{
+    return ((unsigned int)buf[1]<<8) | buf[0];
 }
 
 /*
 ** unpacki32() -- unpack a 32-bit int from a char buffer (like ntohl())
 */ 
-long int unpacki32(unsigned char *buf)
+long int unpacki32(unsigned char *buf, const unsigned char bytodr)
+{
+    long int i;
+
+    if(bytodr == '<'){
+	i = unpacki32_little(buf);
+    } else {
+	i = unpacki32_big(buf);
+    }
+
+    return i;
+}
+
+long int unpacki32_big(unsigned char *buf)
 {
     unsigned long int i2 = ((unsigned long int)buf[0]<<24) |
                            ((unsigned long int)buf[1]<<16) |
@@ -196,10 +264,40 @@ long int unpacki32(unsigned char *buf)
     return i;
 }
 
+long int unpacki32_little(unsigned char *buf)
+{
+    unsigned long int i2 = ((unsigned long int)buf[3]<<24) |
+                           ((unsigned long int)buf[2]<<16) |
+                           ((unsigned long int)buf[1]<<8)  |
+                           buf[0];
+    long int i;
+
+    // change unsigned numbers to signed
+    if (i2 <= 0x7fffffffu) { i = i2; }
+    else { i = -1 - (long int)(0xffffffffu - i2); }
+
+    return i;
+}
+
+
 /*
 ** unpacku32() -- unpack a 32-bit unsigned from a char buffer (like ntohl())
 */ 
-unsigned long int unpacku32(unsigned char *buf)
+unsigned long int unpacku32(unsigned char *buf, const unsigned char bytodr)
+{
+    unsigned long int i;
+
+    if(bytodr == '<'){
+	i = unpacku32_little(buf);
+    } else {
+	i = unpacku32_big(buf);
+    }
+
+    return i;
+}
+
+
+unsigned long int unpacku32_big(unsigned char *buf)
 {
     return ((unsigned long int)buf[0]<<24) |
            ((unsigned long int)buf[1]<<16) |
@@ -207,42 +305,125 @@ unsigned long int unpacku32(unsigned char *buf)
            buf[3];
 }
 
+unsigned long int unpacku32_little(unsigned char *buf)
+{
+    return ((unsigned long int)buf[3]<<24) |
+           ((unsigned long int)buf[2]<<16) |
+           ((unsigned long int)buf[1]<<8)  |
+           buf[0];
+}
+
 /*
 ** unpacki64() -- unpack a 64-bit int from a char buffer (like ntohl())
 */ 
-long long int unpacki64(unsigned char *buf)
+long long int unpacki64(unsigned char *buf, const unsigned char bytodr)
 {
-    unsigned long long int i2 = ((unsigned long long int)buf[0]<<56) |
-                                ((unsigned long long int)buf[1]<<48) |
-                                ((unsigned long long int)buf[2]<<40) |
-                                ((unsigned long long int)buf[3]<<32) |
-                                ((unsigned long long int)buf[4]<<24) |
-                                ((unsigned long long int)buf[5]<<16) |
-                                ((unsigned long long int)buf[6]<<8)  |
-                                buf[7];
-    long long int i;
+    unsigned long long int i;
 
-    // change unsigned numbers to signed
-    if (i2 <= 0x7fffffffffffffffu) { i = i2; }
-    else { i = -1 -(long long int)(0xffffffffffffffffu - i2); }
+    if(bytodr == '<') {
+	i = unpacki64_little(buf);
+    } else {
+	i = unpacki64_big(buf);
+    }
 
     return i;
 }
 
+long long int unpacki64_big(unsigned char *buf)
+{
+    unsigned long long int i2;
+    long long int i;
+
+    i2 = ((unsigned long long int)buf[0]<<56) |
+	((unsigned long long int)buf[1]<<48) |
+	((unsigned long long int)buf[2]<<40) |
+	((unsigned long long int)buf[3]<<32) |
+	((unsigned long long int)buf[4]<<24) |
+	((unsigned long long int)buf[5]<<16) |
+	((unsigned long long int)buf[6]<<8)  |
+	((unsigned long long int)buf[7]);
+
+    // change unsigned numbers to signed
+    if (i2 <= 0x7fffffffffffffffu) {
+	i = i2;
+    } else {
+	i = -1 -(long long int)(0xffffffffffffffffu - i2);
+    }
+
+    return i;
+
+}
+
+
+long long int unpacki64_little(unsigned char *buf)
+{
+    unsigned long long int i2 = ((unsigned long long int)buf[7]<<56) |
+	((unsigned long long int)buf[6]<<48) |
+	((unsigned long long int)buf[5]<<40) |
+	((unsigned long long int)buf[4]<<32) |
+	((unsigned long long int)buf[3]<<24) |
+	((unsigned long long int)buf[2]<<16) |
+	((unsigned long long int)buf[1]<<8)  |
+	((unsigned long long int)buf[0]);
+    long long int i;
+
+    // change unsigned numbers to signed
+    if (i2 <= 0x7fffffffffffffffu) {
+	i = i2;
+    } else {
+	i = -1 -(long long int)(0xffffffffffffffffu - i2);
+    }
+
+    return i;
+}
+
+
 /*
 ** unpacku64() -- unpack a 64-bit unsigned from a char buffer (like ntohl())
 */ 
-unsigned long long int unpacku64(unsigned char *buf)
+unsigned long long int unpacku64(unsigned char *buf, const unsigned char bytodr)
 {
-    return ((unsigned long long int)buf[0]<<56) |
-           ((unsigned long long int)buf[1]<<48) |
-           ((unsigned long long int)buf[2]<<40) |
-           ((unsigned long long int)buf[3]<<32) |
-           ((unsigned long long int)buf[4]<<24) |
-           ((unsigned long long int)buf[5]<<16) |
-           ((unsigned long long int)buf[6]<<8)  |
-           buf[7];
+    unsigned long long int i;
+    
+    if(bytodr == '<') {
+	i = unpacku64_little(buf);
+    } else {
+	i = unpacku64_big(buf);
+    }
+
+    return i;
 }
+
+long long int unpacku64_little(unsigned char *buf)
+{
+    unsigned long long int i = ((unsigned long long int)buf[7]<<56) |
+	((unsigned long long int)buf[6]<<48) |
+	((unsigned long long int)buf[5]<<40) |
+	((unsigned long long int)buf[4]<<32) |
+	((unsigned long long int)buf[3]<<24) |
+	((unsigned long long int)buf[2]<<16) |
+	((unsigned long long int)buf[1]<<8)  |
+	((unsigned long long int)buf[0]);
+
+    return i;
+}
+
+long long int unpacku64_big(unsigned char *buf)
+{
+    unsigned long long int i;
+
+    i =  ((unsigned long long int)buf[0]<<56) |
+	((unsigned long long int)buf[1]<<48) |
+	((unsigned long long int)buf[2]<<40) |
+	((unsigned long long int)buf[3]<<32) |
+	((unsigned long long int)buf[4]<<24) |
+	((unsigned long long int)buf[5]<<16) |
+	((unsigned long long int)buf[6]<<8)  |
+	buf[7];
+
+    return i;
+}
+
 
 /*
 ** pack() -- store data dictated by the format string in the buffer
@@ -413,6 +594,11 @@ unsigned int beej_pack(unsigned char *buf, char *format, ...)
 /*
 ** unpack() -- unpack data dictated by the format string into the buffer
 **
+**   bits |byte order          float      alignment
+**   -----+------------------------------------------
+**      < |   little-endian    standard     none
+**      > |   big-endian       standard     none
+**
 **   bits |signed   unsigned   float   string
 **   -----+----------------------------------
 **      8 |   c        C         
@@ -448,10 +634,21 @@ void beej_unpack(unsigned char *buf, char *format, ...)
     char *s;
     unsigned int len, maxstrlen=0, count;
 
+    unsigned char bytodr = NUL_BYT_ORD;
+
     va_start(ap, format);
 
     for(; *format != '\0'; format++) {
         switch(*format) {
+	    
+	case '>': // big-endian
+	    bytodr = '>';
+	    break;
+	    
+	case '<': // little-endian
+	    bytodr = '<';
+	    break;
+	    
         case 'c': // 8-bit
             c = va_arg(ap, signed char*);
             if (*buf <= 0x7f) { *c = *buf;} // re-sign
@@ -466,64 +663,64 @@ void beej_unpack(unsigned char *buf, char *format, ...)
 
         case 'h': // 16-bit
             h = va_arg(ap, int*);
-            *h = unpacki16(buf);
+            *h = unpacki16(buf, bytodr);
             buf += 2;
             break;
 
         case 'H': // 16-bit unsigned
             H = va_arg(ap, unsigned int*);
-            *H = unpacku16(buf);
+            *H = unpacku16(buf, bytodr);
             buf += 2;
             break;
 
         case 'l': // 32-bit
             l = va_arg(ap, long int*);
-            *l = unpacki32(buf);
+            *l = unpacki32(buf, bytodr);
             buf += 4;
             break;
 
         case 'L': // 32-bit unsigned
             L = va_arg(ap, unsigned long int*);
-            *L = unpacku32(buf);
+            *L = unpacku32(buf, bytodr);
             buf += 4;
             break;
 
         case 'q': // 64-bit
             q = va_arg(ap, long long int*);
-            *q = unpacki64(buf);
+            *q = unpacki64(buf, bytodr);
             buf += 8;
             break;
 
         case 'Q': // 64-bit unsigned
             Q = va_arg(ap, unsigned long long int*);
-            *Q = unpacku64(buf);
+            *Q = unpacku64(buf, bytodr);
             buf += 8;
             break;
 
         case 'f': // float
             f = va_arg(ap, float*);
-            fhold = unpacku16(buf);
+            fhold = unpacku16(buf, bytodr);
             *f = unpack754_16(fhold);
             buf += 2;
             break;
 
         case 'd': // float-32
             d = va_arg(ap, double*);
-            fhold = unpacku32(buf);
+            fhold = unpacku32(buf, bytodr);
             *d = unpack754_32(fhold);
             buf += 4;
             break;
 
         case 'g': // float-64
             g = va_arg(ap, long double*);
-            fhold = unpacku64(buf);
+            fhold = unpacku64(buf, bytodr);
             *g = unpack754_64(fhold);
             buf += 8;
             break;
 
         case 's': // string
             s = va_arg(ap, char*);
-            len = unpacku16(buf);
+            len = unpacku16(buf, bytodr);
             buf += 2;
             if (maxstrlen > 0 && len > maxstrlen) count = maxstrlen - 1;
             else count = len;
--- protocols/debug_beej_pack.c
+++ protocols/debug_beej_pack.c
@@ -15,8 +15,12 @@ int main(void)
     unsigned char buf[1024];
     int8_t magic;
     int16_t monkeycount = 0x2345;
-    int32_t altitude = 0x12345678;
-    int64_t sg = 0x1122334455667788;
+    int16_t mk_v;
+    int32_t altitude = 0x12;
+    int32_t alt_v;
+    //int64_t sg = -0x122334455667788;
+    int64_t sg = -0x12;
+    int64_t sgv;
     float32_t absurdityfactor;
     char *s = "Great unmitigated Zot!  You've found the Runestaff!";
     char s2[96];
@@ -35,10 +39,13 @@ int main(void)
     /* 	   " \"%s\" %f\n", magic, ps2, monkeycount, */
     /* 	   altitude, s2, absurdityfactor); */
 
-    packetsize = beej_pack(buf, "<h", monkeycount);
+    packetsize = beej_pack(buf, "<H", monkeycount);
     printf("monkeycount packetsize: %d\n", packetsize);
     printf("monkeycount <h[0]:0x%x\n", *(buf+0));
     printf("monkeycount <h[1]:0x%x\n", *(buf+1));
+    beej_unpack(buf, "<H", &mk_v);
+    printf("int16_t alt_v: 0x%x\n", mk_v);
+    printf("int16_t alt_v: %i\n", mk_v);
 
     packetsize = beej_pack(buf, "<L", altitude);
     printf("altitude packetsize: %d\n", packetsize);
@@ -47,16 +54,51 @@ int main(void)
     printf("altitude <h[2]:0x%x\n", *(buf+2));
     printf("altitude <h[3]:0x%x\n", *(buf+3));
 
+    beej_unpack(buf, "L", &alt_v);
+    printf("int32_t alt_v: 0x%x\n", alt_v);
+    printf("int32_t alt_v: %i\n", alt_v);
+    
+
     packetsize = beej_pack(buf, "<q", sg);
     printf("sg packetsize: %d\n", packetsize);
-    printf("sg <h[0]:0x%x\n", *(buf+0));
-    printf("sg <h[1]:0x%x\n", *(buf+1));
-    printf("sg <h[2]:0x%x\n", *(buf+2));
-    printf("sg <h[3]:0x%x\n", *(buf+3));
-    printf("sg <h[4]:0x%x\n", *(buf+4));
-    printf("sg <h[5]:0x%x\n", *(buf+5));
-    printf("sg <h[6]:0x%x\n", *(buf+6));
-    printf("sg <h[7]:0x%x\n", *(buf+7));
+    printf("sg <q[0]:0x%x\n", *(buf+0));
+    printf("sg <q[1]:0x%x\n", *(buf+1));
+    printf("sg <q[2]:0x%x\n", *(buf+2));
+    printf("sg <q[3]:0x%x\n", *(buf+3));
+    printf("sg <q[4]:0x%x\n", *(buf+4));
+    printf("sg <q[5]:0x%x\n", *(buf+5));
+    printf("sg <q[6]:0x%x\n", *(buf+6));
+    printf("sg <q[7]:0x%x\n", *(buf+7));
+
+    packetsize = beej_pack(buf, "q", sg);
+    printf("sgv packetsize: %d\n", packetsize);
+    printf("sgv >q[0]:0x%x\n", *(buf+0));
+    printf("sgv >q[1]:0x%x\n", *(buf+1));
+    printf("sgv >q[2]:0x%x\n", *(buf+2));
+    printf("sgv >q[3]:0x%x\n", *(buf+3));
+    printf("sgv >q[4]:0x%x\n", *(buf+4));
+    printf("sgv >q[5]:0x%x\n", *(buf+5));
+    printf("sgv >q[6]:0x%x\n", *(buf+6));
+    printf("sgv >q[7]:0x%x\n", *(buf+7));
+
+    beej_unpack(buf, "q", &sgv);
+    printf("int64_t sgv: 0x%llx\n", sgv);
+    printf("int64_t sgv: %lli\n", sgv);
+
+    packetsize = beej_pack(buf, "<q", sg);
+    printf("sgv packetsize: %d\n", packetsize);
+    printf("sgv <q[0]:0x%x\n", *(buf+0));
+    printf("sgv <q[1]:0x%x\n", *(buf+1));
+    printf("sgv <q[2]:0x%x\n", *(buf+2));
+    printf("sgv <q[3]:0x%x\n", *(buf+3));
+    printf("sgv <q[4]:0x%x\n", *(buf+4));
+    printf("sgv <q[5]:0x%x\n", *(buf+5));
+    printf("sgv <q[6]:0x%x\n", *(buf+6));
+    printf("sgv <q[7]:0x%x\n", *(buf+7));
+
+    beej_unpack(buf, "<q", &sgv);
+    printf("int64_t sgv: 0x%llx\n", sgv);
+    printf("int64_t sgv: %lli\n", sgv);
     
 
     return 0;
